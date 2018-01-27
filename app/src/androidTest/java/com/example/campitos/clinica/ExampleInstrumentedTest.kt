0ca79f7b90411b2444bc3c3eef3a113a1decfafb
como-quesito <rapidclimate@outlook.com>
2018-01-26 18:01:13 -0600

    Primero

---
+++ app/src/androidTest/java/com/example/campitos/clinica/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.example.campitos.clinica
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.example.campitos.clinica", appContext.packageName)
+    }
+}
